acc7561409342e5579e26e3b7b64af1f389517db
Mai Vang <vmaineng@gmail.com>
2022-07-04 20:18:40 -0500

    commit tree e3ea928e284086389bf8cd8df400f4a666b9e0f6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+rescue_from ActiveRecord::RecordNotFound, with: :render_user_not_found
+
+private
+def render_user_not_found(exception)
+    render json: { error: "#{exception.model} not found" }, status: :not_found
+end
+
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,38 @@
 class ItemsController < ApplicationController
 
   def index
+    if params[:user_id]
+      user = find_user
+      items = user.items
+    else
     items = Item.all
+    end
     render json: items, include: :user
   end
+
+  def show
+  item = find_item
+  render json: item
+  end
+
+  def create
+    user = find_user
+    item = user.items.create(item_params)
+    render json: item, status: :created
+  end
+
+
+private
+def find_item
+    Item.find(params[:id])
+end
+
+def find_user
+  user = User.find(params[:user_id])
+end
+
+def item_params
+  params.permit(:name, :description, :price )
+end
 
 end
